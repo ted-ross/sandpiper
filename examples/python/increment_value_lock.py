913eae781afcd117b198770efb35e27e12d6c064
Ted Ross <tross@redhat.com>
2017-09-12 20:13:36 -0400

    Added lock library, lock example, and updated .gitignore

--- examples/python/increment_value_lock.py
+++ examples/python/increment_value_lock.py
@@ -21,22 +21,36 @@
 from proton.handlers import MessagingHandler
 from proton.reactor import Container
 from sandpiper.request_response import RequestClient
+from sandpiper.lock import Lock
 
-class IncrementValue(MessagingHandler):
-    def __init__(self, bus_address, service_address):
-        super(IncrementValue, self).__init__()
+class IncrementValueLock(MessagingHandler):
+    def __init__(self, bus_address, service_address, lock_name):
+        super(IncrementValueLock, self).__init__()
         self.bus_address     = bus_address
         self.service_address = service_address
+        self.lock_name       = lock_name
 
     def on_start(self, event):
         self.container = event.container
         self.conn      = self.container.connect(self.bus_address)
         self.client    = RequestClient(self.container, self.conn, self.service_address)
+        self.lock      = Lock(self.container, self.conn, self.lock_name)
         self.sent_get  = False
+        self.lock_held = False
 
     def on_service_ready(self, event):
         if not self.sent_get:
-            self.client.request({'opcode':'GET'}, user_context="Read")
+            self.lock.acquire()
+
+    def on_lock_acquired(self, event):
+        self.lock_held = True
+        self.client.request({'opcode':'GET'}, user_context="Read")
+
+    def on_lock_failed(self, event):
+        print("Lock Failed")
+
+    def on_lock_released(self, event):
+        self.lock_held = False
 
     def on_request_failed(self, event):
         pass
@@ -48,11 +62,13 @@ class IncrementValue(MessagingHandler):
                 self.client.request({'opcode':'SET', 'value':(props['value'] + 1)})
             elif props['opcode'] == 'SET':
                 print("Value set to %d" % props['value'])
+                self.lock.release()
+                self.lock.destroy()
                 self.client.stop()
                 self.conn.close()
         except Exception, e:
             print("EXCEPTION: %r" % e)
 
 
-Container(IncrementValue("127.0.0.1:5672", "counterValue")).run()
+Container(IncrementValueLock("127.0.0.1:5672", "counterValue", "mutex.counterValue")).run()
 
